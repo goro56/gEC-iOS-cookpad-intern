fe18c1fd78d8c2a4562fcbaee7cfda5ab8515ba3
Hayato Shimoji <hayato-shimoji@cookpad.com>
2016-08-16 11:29:54 +0900

    created recommended list view

---
+++ Market/Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Market
+//
+//  Created by hayato-shimoji on 2016/08/16.
+//  Copyright © 2016年 COOKPAD inc,. All rights reserved.
+//
+
+import Foundation
+
+struct Item {
+    let id: Int
+    let name: String
+    let desc: String
+    let price: Int
+    let imageURL: NSURL
+}
---
+++ Market/View/RecommendedItemCell.swift
@@ -0,0 +1,22 @@
+//
+//  RecommendedItemCell.swift
+//  Market
+//
+//  Created by hayato-shimoji on 2016/08/16.
+//  Copyright © 2016年 COOKPAD inc,. All rights reserved.
+//
+
+import UIKit
+
+class RecommendedItemCell: UITableViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func update(withItem item: Item) {
+        nameLabel.text = item.name
+        priceLabel.text = "\(item.price)円"
+        descriptionLabel.text = item.desc
+    }
+}
